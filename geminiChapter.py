47db6deafb83823ae90ef82f26d13e62e0719144
Pablo Garcia <pgarciag566@gmail.com>
2025-10-10 17:39:53 -0600

    is it uploaded?

--- geminiChapter.py
+++ geminiChapter.py
@@ -5,6 +5,7 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 import os
 import hashlib
+#Test to see if its uploaded
 
 def check_password():
     """Returns True if the user had the correct password."""
